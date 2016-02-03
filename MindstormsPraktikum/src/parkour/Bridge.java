e3f6e545b5604a5e6916928475ed2b4b201c10d2
ThePrimus <patrick.petersen91@googlemail.com>
2016-02-03 15:35:26 +0100

    Changed port for sonic motor to D port. Added basic test for bridge

--- MindstormsPraktikum/src/parkour/Bridge.java
+++ MindstormsPraktikum/src/parkour/Bridge.java
@@ -1,5 +1,8 @@
 package parkour;
 
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.utility.Delay;
 import logic.Drive;
 
 /**
@@ -21,5 +24,37 @@ public class Bridge {
 	 */
 	public Bridge(Drive drive) {
 		this.drive = drive;
+	}
+
+
+
+	public Bridge(Drive drive2, EV3MediumRegulatedMotor sonicMotor) {
+	//	float pos = sonicMotor.getPosition();
+	//	for(int i = 0; i <= 1000; i++) {
+	//		pos = sonicMotor.getPosition();
+		//	LCD.drawString(String.valueOf(pos), 0, 0);
+		//	Delay.msDelay(2000);
+		//	LCD.clear();
+	//	}
+		
+		for(int i = 0; i <= 10; i++){
+			sonicMotor.rotate(1);
+			LCD.drawString(String.valueOf(sonicMotor.getPosition()), 0, 0);
+			Delay.msDelay(1000);
+			LCD.clear();
+		}
+	
+	
+	//	sonicMotor.setSpeed(200);
+		//sonicMotor.rotateTo(0);
+		//Delay.msDelay(1000);
+		//sonicMotor.rotate(90);
+/*
+		Delay.msDelay(2000);
+		sonicMotor.rotateTo(180);
+
+		Delay.msDelay(2000);
+		sonicMotor.rotate(90);
+		*/
 	}
 }
