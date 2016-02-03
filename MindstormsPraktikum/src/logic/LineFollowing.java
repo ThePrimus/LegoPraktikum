3accd4975a8253f23d81e54168ca777927b16229
Daniel Kosinski <daniel.kosinski1991@gmail.com>
2016-02-03 23:58:48 +0100

    Prototype of line following. Are turnLeft etc blockng?

--- MindstormsPraktikum/src/logic/LineFollowing.java
+++ MindstormsPraktikum/src/logic/LineFollowing.java
@@ -3,6 +3,8 @@ package logic;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.filter.OffsetCorrectionFilter;
 
+import java.lang.Math;
+
 /**
  * Lets the robot follow a path (line on ground).
  * 
@@ -36,36 +38,77 @@ public class LineFollowing {
 		sensor.setFloodlight(lejos.robotics.Color.RED);
 	}
 	
-	private void FindMaxIntensity(boolean rightMoves){
-		float maxIntensity = 0;
+	/**
+	 * Finds the max value for red intesity by rotating to 90° and -90°
+	 */
+	private void FindMaxIntensity(){
+		float redMaxR = 0;
+		float redMaxL = 0;
 		boolean movment = true;
 		float [] samples = new float[sensor.sampleSize()];
 		
-		if(rightMoves){
-			drive.moveForward(0, drive.maxSpeed());
-		}
-		else
-		{
-			drive.moveForward(drive.maxSpeed(), 0);
-		}
-		
+		drive.turnRight(90);
+				
 		while(movment){
-			if(rightMoves)
-			{
-				movment = drive.isRightMoving();
-			}else{
-				movment = drive.isLeftMoving();
-			}
+			movment = drive.isLeftMoving() && drive.isRightMoving();
 			sensor.fetchSample(samples, 0);
 			
-			redMax = redMax < samples[0] ? samples[0] : redMax;
+			redMaxR = redMaxR < samples[0] ? samples[0] : redMaxR;
 		}
+		movment = true;
+		drive.turnLeft(180);
 		
+		while(movment){
+			movment = drive.isLeftMoving() && drive.isRightMoving();
+			sensor.fetchSample(samples, 0);
+			
+			redMaxL = redMaxL < samples[0] ? samples[0] : redMaxL;
+		}
+		drive.turnRight(90);
+		
+		redMax = (redMaxR + redMaxL)/2;
 	}
-	private void Calibrate(){
-		
+	
+	public void FollowLine(){
+		FindMaxIntensity();
 		filter = new OffsetCorrectionFilter(sensor);
 		float[] sample = new float[filter.sampleSize()];
+		float lastSample = 0;
+		boolean rightTurn = false;
+		int count = 0;
+		
+		drive.moveForward(drive.maxSpeed(), drive.maxSpeed());
+		
+		while(drive.isLeftMoving() && drive.isRightMoving())
+		{	
+			filter.fetchSample(sample, 0);
+			
+			if(sample[0] < (redMax * 0.8) || !rightTurn){
+				while(sample[0] < redMax * 0.8)
+				{
+					filter.fetchSample(sample, 0);
+					drive.turnRight(5);
+				}
+				rightTurn = true;
+			}
+			
+			if(sample[0] < (redMax * 0.8) || rightTurn){
+				while(sample[0] < (redMax * 0.8))
+				{
+					filter.fetchSample(sample, 0);
+					drive.turnLeft(5);
+					rightTurn = false;
+				}
+			}
+			if(((sample[0] - lastSample) <= 0.0f ? 0.0f - (sample[0] - lastSample) : (sample[0] - lastSample)) < 0.2 && sample[0] < redMax * 0.2){
+				count++;
+			}
+			if(count == 100)
+			{
+				drive.stop();
+			}
+		}
+		
 	}
 		
 }
