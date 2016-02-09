ae3dba784627e35d2172d102728a1b51efe7bf3f
Daniel Kosinski <daniel.kosinski1991@gmail.com>
2016-02-09 14:29:04 +0100

    Parameter tunning, Line Search added

--- MindstormsPraktikum/src/logic/LineFollowing.java
+++ MindstormsPraktikum/src/logic/LineFollowing.java
@@ -3,13 +3,7 @@ package logic;
 import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.robotics.RegulatedMotor;
-import lejos.robotics.RegulatedMotorListener;
 import lejos.robotics.SampleProvider;
-import lejos.robotics.filter.MedianFilter;
-import lejos.robotics.filter.SampleThread;
-import lejos.utility.Delay;
-
 /**
  * Lets the robot follow a path (line on ground).
  * 
@@ -29,12 +23,10 @@ public class LineFollowing {
 	private float mSpeed;
 	private final float diffSpeed = 140; //140 oder 100 mit emplified
 	private float initSpeed; //= mSpeed - 2 * diffSpeed;
+	
 	private float timestamp = 0;
 	
-	private int tachoCount = 0;
-	
 	private boolean terminate = false;
-	//private char lastState = 's';
 	
 	/**
 	 * Constructor: 
@@ -60,7 +52,7 @@ public class LineFollowing {
 		int inc = 10;
 		float [] samples = new float[colorProvider.sampleSize()];
 		float curVal  = 0;
-		while(deg < 120) {
+		while(deg < 90) {
 				drive.turnLeft(-inc, false);
 				deg += inc;
 				colorProvider.fetchSample(samples, 0);
@@ -76,74 +68,7 @@ public class LineFollowing {
 	return found;
 	}
 	
-	/**
-	 * Executes an algorithm so that the robot follows a silver/white line.
-	 * Idea: Stay on line and adjust depending on last adjustment.
-	 * Problem: Parameterization of states and state switching depends on sample fetching frequency
-	 */
-	/*
-	public void runt(){
-		LCD.clear();
 
-		char state = 'f';
-				
-		SampleThread sample = new SampleThread(sensor, 2);
-		float[] samples = new float[sample.sampleSize()];
-		
-		while(!terminate) {
-			if(sample.isNewSampleAvailable())
-			{
-				sample.fetchSample(samples, 0);
-				
-				LCD.drawString(String.valueOf("Sample: " + String.valueOf(samples[0])), 0, 0);
-				LCD.drawString("Last State: " + lastState, 0, 2);
-				LCD.drawString("State: " + state, 0, 3);
-				
-				if(samples[0] < 0.2)
-				{
-					switch(lastState) {
-					case 'r':
-						//sample.fetchSample(samples, 0);
-						//LCD.drawString("Sample: " + String.valueOf(samples[0]), 0, 4);
-						if(state == 'f') {
-							drive.moveForward(0, drive.maxSpeed() * 0.3f);
-							lastState = 'l';
-						}
-						//LCD.drawString("State: " + lastState, 0, 3);
-						//drive.setSpeedRightMotor(drive.maxSpeed());
-						
-						break;
-					case 'l':
-						//Sound.beep();
-						//sensor.fetchSample(samples, 0);
-						//LCD.drawString("Sample: " + String.valueOf(samples[0]), 0, 4);
-						if(state == 'f') {
-							lastState = 'r';
-							drive.moveForward(drive.maxSpeed() * 0.3f, 0);
-						}
-						//LCD.drawString("State: " + lastState, 0, 3);
-						
-						break;
-					case 's':
-						searchLine();
-						break;
-					}
-					//lastSample = samples[0];
-				
-				} 
-				
-				state = 'n';
-				
-				if(samples[0] > 0.8) {
-					state = 'f';
-					drive.moveForward(drive.maxSpeed()* 0.2f, drive.maxSpeed()* 0.2f);
-				}
-				
-			}
-		}
-		drive.stop();
-	}
-	*/
 	/**
 	 * Executes an algorithm so that the robot follows a silver/white line.
 	 * Idea: Adjust the whole time to reach a red intensity between 0.5 - 0.4
@@ -158,14 +83,14 @@ public class LineFollowing {
 			// get color of line
 			if(terminate) {
 				Sound.beep();
-				drive.stop();
+				drive.stopSynchronized();
 				//terminate = true;
 				break;
 			}
 			if(counter > 100000) {
 				Sound.twoBeeps();
 				terminate = true;
-				drive.stop();
+				drive.stopSynchronized();
 				break;
 			}
 			
@@ -181,7 +106,6 @@ public class LineFollowing {
 				drive.leftBackward(lSpeed);
 			} else {
 				timestamp = 0;
-				tachoCount = 0;
 				drive.setSpeedLeftMotor(lSpeed);
 			}
 
@@ -194,7 +118,7 @@ public class LineFollowing {
 					//Sound.beep();
 					
 				} else if(((System.nanoTime() - timestamp) / 1000000000.0f) > 1.4) {
-					drive.stop();
+					drive.stopSynchronized();
 					Sound.beep();
 					//LCD.drawString("Break!", 0, 5);
 					drive.turnLeft(-90, false);
@@ -204,37 +128,17 @@ public class LineFollowing {
 						break;
 					}
 					
-					//Delay.msDelay(000);
-					/*while(!lineFound()) {
-						if(search > 18) {
-							LCD.drawString("Line not Found!", 0, 1);
-							deg = 0;
-							break;
-						}
-						LCD.drawString("Looking for Line...", 0, 1);
-						search++;
-					}*/
+					
 				}
 				drive.setSpeedRightMotor(rSpeed);
 			}
 			
-			//LCD.drawString("Dif: " + String.valueOf(Math.abs(lastSample - curColor)), 0, 2);
-			//LCD.drawString("Left: " + String.valueOf(lSpeed), 0, 3);
-			//LCD.drawString( "Right: " + String.valueOf(lSpeed), 0, 2);
+			
 			counter++;
-			//lastSample = curColor;
-			//LCD.drawString("Timestamp: " + String.valueOf(timestamp), 0, 2);
-			//LCD.drawString("Dif: " + String.valueOf(timestamp - System.currentTimeMillis()), 0, 4);
-			/*System.out.println();
-			System.out.println("Dif: " + String.valueOf(timestamp - System.currentTimeMillis()));*/
-		}
-		//}
+			
+	}
 		
-		//Delay.msDelay(1000);
-		//searchLine();
 		
-		// ToDo: Move code to correct position: seesaw obstacle finished, inform GUI to start
-		// the search for a barcode
 		GUI.PROGRAM_FINISHED_START_BARCODE = false;
 		terminate = false;
 		timestamp = 0;
@@ -242,7 +146,7 @@ public class LineFollowing {
 	
 	
 	public void end() {
-		drive.stop();
+		drive.stopSynchronized();
 		this.terminate = true;
 	}
 	
